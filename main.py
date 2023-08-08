5a6e8508f8e2716e82c836d4781903e2b6b761c5
“Valeria <valeriacarvajal1@outlook.com>
2023-08-08 16:19:39 -0600

    Avances Val 8-8-23

--- main.py
+++ main.py
@@ -42,75 +42,99 @@ class ExploradorDeArchivos(QtWidgets.QDialog):
         uic.load_ui.loadUi("UI_Explorador.ui", self)
         self.ruta= os.path.dirname(__file__) + '\\Raiz' #Creamos una ruta raiz que queremos mostrar en nuestra aplicación
         
-        #Creamos una variable Modelo en la cual vamos a instanciar la clase QFileSystemModel que
-        #proporcional un modelo de datos para archvios locales
-        self.modelo = QFileSystemModel()
-        #Creamos una variable de ruta para guardar la ruta raíz que queremos mostrar en la ventana
         
-        #Establecemos la ruta raiz para mostrar en el QTreeView
-        self.modelo.setRootPath(self.ruta)
-        self.txtDir = self.findChild(QtWidgets.QLineEdit, "txtDirectorio")
-        self.txtDir.setText(self.ruta) #Inicialmente se mostrará la ruta raiz en la pantalla
-
         
-        #Incializamos una variable de tipo QTreeView que se encuentra en al archivo .ui
-        self.arbol = self.findChild(QtWidgets.QTreeView, "treeArbol")
-        
-        #SetModel() es un método heredado de la clase "QAbstractItemModel Class" que proporciona una clase
-        #abstracta para las clases de modelos de elemetnos. Como QTreeview es un elemento que requiere
-        #items, podemos manipular su modelo de intem con la función setModel()
-        self.arbol.setModel(self.modelo)
-        self.arbol.setColumnWidth(0, 250) #Expando la columna de nombre de archivos/directorios
-        #Con setRootIndex() se establece el elemento de ruta raiz: https://doc.qt.io/qt-6/qabstractitemview.html#setRootIndex
-        #Con el método index() de la clase QFileSystemModel retornar el item del modelo con la ruta establecida como ruta raíz
-        self.arbol.setRootIndex(self.modelo.index(self.ruta))
-        self.filtro()#
         self.explorador_rapido()
-        self.arbol.doubleClicked.connect(self.abrir_archivo)
+        self.explorador_secundario()
+        self.arbol.clicked.connect(self.actualizar_explorador_secundario)
+        self.arbol2.doubleClicked.connect(self.abrir_directorio)
         self.texto = None
         
         self.btnFolder= self.findChild(QtWidgets.QPushButton, "btnCrearCarpeta")
         self.btnFolder.clicked.connect(self.abrir_ventana)
+        self.btnReinic= self.findChild(QtWidgets.QPushButton, "btnReiniciar")
         
+     
     def explorador_rapido(self):
-        self.arbol2 = self.findChild(QtWidgets.QTreeView, "arbol2")
-        self.arbol2.setModel(self.modelo)
-        self.arbol2.setRootIndex(self.modelo.index(self.ruta))
-        self.arbol2.hideColumn(1)
-        self.arbol2.hideColumn(2)
-        self.arbol2.hideColumn(3)
+        #Creamos una variable Modelo en la cual vamos a instanciar la clase QFileSystemModel que
+        #proporcional un modelo de datos para archvios locales
+        modelo1 = QFileSystemModel()
+        #Creamos una variable de ruta para guardar la ruta raíz que queremos mostrar en la ventana
         
-    def abrir_archivo(self,index):
+        #Establecemos la ruta raiz para mostrar en el QTreeView
+        modelo1.setRootPath(self.ruta)
+        
+        self.arbol = self.findChild(QtWidgets.QTreeView, "arbol2")
+        self.arbol.setModel(modelo1)
+        self.arbol.setRootIndex(modelo1.index(self.ruta))
+        self.arbol.hideColumn(1)
+        self.arbol.hideColumn(2)
+        self.arbol.hideColumn(3)
+        self.filtro(modelo1,"")
+        
+    def explorador_secundario(self, ruta_2 = os.path.dirname(__file__) + '\\Raiz'):
+        #Creamos una variable Modelo en la cual vamos a instanciar la clase QFileSystemModel que
+        #proporcional un modelo de datos para archvios locales
+        self.modelo2 = QFileSystemModel()
+        #Creamos una variable de ruta para guardar la ruta raíz que queremos mostrar en la ventana
+        
+        #Establecemos la ruta raiz para mostrar en el QTreeView
+        self.modelo2.setRootPath(ruta_2)
+        
+        #Incializamos una variable de tipo QTreeView que se encuentra en al archivo .ui
+        self.arbol2 = self.findChild(QtWidgets.QTreeView, "treeArbol")
+        
+        #SetModel() es un método heredado de la clase "QAbstractItemModel Class" que proporciona una clase
+        #abstracta para las clases de modelos de elemetnos. Como QTreeview es un elemento que requiere
+        #items, podemos manipular su modelo de intem con la función setModel()
+        self.arbol2.setModel(self.modelo2)
+        self.arbol2.setColumnWidth(0, 250) #Expando la columna de nombre de archivos/directorios
+        #Con setRootIndex() se establece el elemento de ruta raiz: https://doc.qt.io/qt-6/qabstractitemview.html#setRootIndex
+        #Con el método index() de la clase QFileSystemModel retornar el item del modelo con la ruta establecida como ruta raíz
+        self.arbol2.setRootIndex(self.modelo2.index(ruta_2))
+        #Cuadro de texto con directorio
+        self.txtDir = self.findChild(QtWidgets.QLineEdit, "txtDirectorio")
+        self.txtDir.setText(ruta_2) #Inicialmente se mostrará la ruta raiz en la pantalla
+        self.filtro(self.modelo2,"*.txt")#
+        
+    def actualizar_explorador_secundario(self, index):
+        ruta_nueva= self.modelo2.filePath(index)
+        self.explorador_secundario(ruta_nueva)
+    
+    def abrir_directorio(self,index):
         
         try:
-           
-            archivo = open(self.modelo.filePath(index), 'r')
-            texto= archivo.read()
-            archivo.close()
-            ventanaArchivo(texto).exec()
+            ruta_nueva = self.modelo2.filePath(index)
+            if os.path.isfile(ruta_nueva):
+                archivo = open(self.modelo2.filePath(index), 'r')#Agarrro la ruta del directorio que le di doble click
+                texto= archivo.read()
+                archivo.close()
+                ventanaArchivo(texto).exec()
+            else:
+                self.explorador_secundario(ruta_nueva)
 
         except OSError:
             print("El archivo no se pudo abrir \n", OSError.strerror) #Mandamos un mensaje en consola e imprimimos el detalle del error
             #archivo.close()
-        except UnboundLocalError:
+        except UnboundLocalError: #Erro al abrir una carpeta como si duera archivo
             pass
         except BaseException:
-            archivo.close()
-            
+            archivo.close()          
     
-    def filtro(self):#Con este método filtramos solo archivos .txt aparezcan
-        filtro = ["*.txt"]#Tipo de archivo con extensión .txt que deseamos mostrar, en formato lista
-        self.modelo.setNameFilters(filtro)#Añadimos el fltro recibido como lista
-        self.modelo.setNameFilterDisables(False)#Activamos el filro
+    def filtro(self, model, tipo):#Con este método filtramos solo archivos .txt aparezcan
+        filtro = [tipo]#Tipo de archivo con extensión .txt que deseamos mostrar, en formato lista
+        model.setNameFilters(filtro)#Añadimos el fltro recibido como lista
+        model.setNameFilterDisables(False)#Activamos el filro
     
     def crear_directorio(self,nombre):#Este método es para crear una nueva carpeta
         try:
-            self.newPath= os.path.join(self.ruta, nombre) #Establecemos la ruta de la carpeta más el nombre de la misma en una variabel str
+            self.newPath= os.path.join(self.txtDir.text(), nombre) #Establecemos la ruta de la carpeta más el nombre de la misma en una variabel str
+            print("cuadro: " ,self.txtDir.text())
+            print("path: " ,self.modelo2.rootPath())
             self.folder = os.makedirs(self.newPath) #Creamos la carpeta con el método mkdirs() del módulo os con su número de modelo
         except FileExistsError: #Se reconoce una excepeción el nombre de la carpeta ya existe
             self.mensaje_error_directorio()
             
-
     def mensaje_error_directorio(self):
         mensaje = QtWidgets.QMessageBox(self)
         mensaje.setWindowTitle("Error")
